bc5c9af6946946cbdec2faf8612f53e1a6bea1fc
wendersonalexandre02 <136288467+wendersonalexandre@users.noreply.github.com>
2025-01-29 16:56:49 -0300

    Git ignore

--- Logica de programação com javascript/Funções e listas/app.js
+++ Logica de programação com javascript/Funções e listas/app.js
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag,texto){
 }
 
 function exibirMensagemInicial(){
-    exibirTextoNaTela('h1','Jogo do número secreto');
+    exibirTextoNaTela('h1','Jogo do número secretoo');
     exibirTextoNaTela('p','Escolha um número entre 1 e 50');
 }
 
