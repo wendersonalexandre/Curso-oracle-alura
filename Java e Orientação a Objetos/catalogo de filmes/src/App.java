1901ac82c478099349840147f999036a32228dfc
wendersonalexandre02 <136288467+wendersonalexandre@users.noreply.github.com>
2025-04-03 15:44:15 -0300

    Começando fase 3 do curso com a linguagem Java

---
+++ Java e Orientação a Objetos/catalogo de filmes/src/App.java
@@ -0,0 +1,12 @@
+public class App{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bem-Vindo ao Screen Macth!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022; // Variavel do tipo int 
+        System.out.println("Ano de Lançamento: " + anoDeLancamento); // Exibe o ano de lançamento concatenado com a variavel 
+        boolean incluidoNoPlano = true;
+        
+
+    }
+}
