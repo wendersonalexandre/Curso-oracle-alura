5375378cf3c6f6c41c55fb6dc5897e4a9bcc2c76
wendersonalexandre02 <136288467+wendersonalexandre@users.noreply.github.com>
2025-04-03 22:22:44 -0300

    Finalizando estudos os estudos de java por hoje

--- Java e Orientação a Objetos/catalogo de filmes/src/App.java
+++ Java e Orientação a Objetos/catalogo de filmes/src/App.java
@@ -6,7 +6,21 @@ public class App{
         int anoDeLancamento = 2022; // Variavel do tipo int 
         System.out.println("Ano de Lançamento: " + anoDeLancamento); // Exibe o ano de lançamento concatenado com a variavel 
         boolean incluidoNoPlano = true;
-        
+        System.out.println("Incluido no plano: " + incluidoNoPlano); // Exibe se o filme está incluído no plano concatenado com a variavel
+
+        double media = ( 9.8 + 6.3 + 8.0 ) / 3;
+        System.out.println(media); // Exibe a média concatenada com a variavel
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com Tom Cruise
+                Muito bom
+                ano de lançamento: 
+                """+ anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println("Classificação: " + classificacao); // Exibe a classificação concatenada com a variavel 
 
     }
 }
